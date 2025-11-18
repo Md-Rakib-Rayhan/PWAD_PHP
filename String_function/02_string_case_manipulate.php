555f21cecf775a76e46ffb36b8422935d48b3f6f
Md.Rakib-Rayhan <mdrakibrayhan8@gmail.com>
2025-11-19 00:37:35 +0600

    19-11-2025

--- String_function/02_string_case_manipulate.php
+++ String_function/02_string_case_manipulate.php
@@ -1,10 +1,43 @@
 <?php
 
-$txt = "RakiB";
+    $txt = "RaKiB";
 
-echo strtoupper($txt). "<br>";
-echo strtolower($txt). "<br>";
+    echo strtoupper($txt). "<br>";
+    echo strtolower($txt). "<br>";
+    echo strrev($txt). "<br>";
 
+?>
+
+<hr>
+
+
+<?php
+
+#    0 1 2 3 4 5 6 7 8 9 10 11
+#    H e l l o   W o r l  d  !
+#              ^----- start at index 6 ("W")
+#              take 5 characters → "World"
+
+    $x = "Hello-World!";                  
+    echo substr($x, 6, 5);
+    // substr($string, $start, $length);
+
+    echo "<br>";
+    echo substr($x, 6)."<br>";
+
+#                 -5 -4 -3 -2 -1 
+#    H e l l o   W o  r  l  d  !
+#                  ↑
+#             Start at index -5 (which is "o")
+
+    echo substr($x, -5, 3). "<br>";
+
+#   Start at index 5
+#   Length = -3: Stop 3 characters from the end, which means we stop at index 9.
+#   "Hello World!" is 12 characters, so if we subtract 3 from the end, we get:
+#   12 - 3 = 9 (so, we stop extracting at index 9).
+
+    echo substr($x, 5, -3). "<br>";
 
 
 ?>
