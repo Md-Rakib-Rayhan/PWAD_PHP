0f564f18e514852e7a3dc682b55dd7d9be57ddbf
Md. Rakib Rayhan <mdrakibrayhan8@gmail.com>
2025-11-26 11:12:46 +0600

    26-11

---
+++ Evidence_Text/3_sql_connection_practice/db_config.php
@@ -0,0 +1,14 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "class_test_practice";
+
+
+    $db = new mysqli($host, $user, $password, $database);
+    // echo "Done";
+
+
+
+?>
---
+++ Evidence_Text/3_sql_connection_practice/manufacturer_insert.php
@@ -0,0 +1,57 @@
+<?php  include_once('db_config.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Insert Data in Manufacturer table</h2>
+
+    <form action="" method="post">
+
+        Name <br>
+        <input type="text" name="name" placeholder="Enter name here"> <br>
+        
+        Address <br>
+        <input type="text" name="address" placeholder="Enter address here"> <br>
+        
+        Contact <br>
+        <input type="text" name="contact" placeholder="Enter name here"> <br> <br>
+        
+        <input type="submit" name="submit" value="Submit"> 
+        
+    </form>
+
+
+    <?php
+
+        if(isset($_POST['submit']) && $_POST['name']!= "" && $_POST['address']!= "" && $_POST['contact']!= ""){
+
+            $name = $_POST['name'];
+            $address = $_POST['address'];
+            $contact = $_POST['contact'];
+
+            $sql = "INSERT INTO manufacturer VALUE (NULL, '$name', '$address', '$contact');";
+            $db->query($sql);
+
+            if($db->affected_rows){
+              echo "<h1>Successfully Inserted</h1>";
+              echo "<img src='cat_kiss.gif' width='200px'>";
+            }else{
+              echo "<h1>Problem Occure, Not Inserted</h1>";
+            }
+        }else if(isset($_POST['submit'])){
+            echo "<img src='gif_pic.gif' width='200px'>";
+            echo "<h1>You need to fillup all</h1>";
+        }
+
+
+
+    
+    ?>
+
+</body>
+</html>
